103fb0b79cb787f0b54841d289e6d08a66de1673
Youssef <ys1465@fayoum.edu.eg>
2026-01-26 18:27:27 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/colormate_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.colormate_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ColorMateApp());
+}
+
+class ColorMateApp extends StatelessWidget {
+  const ColorMateApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
