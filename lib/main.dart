48e1ca8c55e109d5acf9161f8d732b52ce3bedb8
Youssef <ys1465@fayoum.edu.eg>
2026-01-30 12:55:05 +0200

    modify main

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@ import 'package:colormate_app/core/utils/app_router.dart';
 import 'package:colormate_app/core/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'core/routing/app_router.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+//import 'core/routing/app_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
