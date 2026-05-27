b8df870ce59745a39f3a7b961eef9220d9de1618
Youssef <ys1465@fayoum.edu.eg>
2026-05-27 20:04:55 +0300

    add user name in welcome section in home screen

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,14 +1,28 @@
 import 'package:colormate_app/features/home/presentation/views/widgets/home_view_body.dart';
+import 'package:colormate_app/features/profile/presentation/cubit/profile_cubit/profile_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeView extends StatelessWidget {
-
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   static const routeName = 'homeView';
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<ProfileCubit>().fetchUserProfile();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return HomeViewBody();
+    return const HomeViewBody();
   }
 }
--- lib/features/home/presentation/views/widgets/welcome_section.dart
+++ lib/features/home/presentation/views/widgets/welcome_section.dart
@@ -1,5 +1,7 @@
 import 'package:colormate_app/core/utils/constants.dart';
+import 'package:colormate_app/features/profile/presentation/cubit/profile_cubit/profile_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class WelcomeSection extends StatelessWidget {
@@ -7,33 +9,42 @@ class WelcomeSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final profileState = context.watch<ProfileCubit>().state;
+
+    final userName = switch (profileState) {
+      ProfileLoaded(:final userProfile) => userProfile.firstName.isNotEmpty
+          ? userProfile.firstName
+          : userProfile.name,
+      ProfileUpdateSuccess(:final userProfile) =>
+        userProfile.firstName.isNotEmpty
+            ? userProfile.firstName
+            : userProfile.name,
+      _ => null,
+    };
+
     return Padding(
       padding: EdgeInsets.only(right: 24.w, left: 24.w, top: 20.h),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            children: [
-              Text(
-                'Welcome back, Mariam 👋',
-                style: TextStyle(
-                  fontSize: 22.sp,
-                  fontWeight: FontWeight.bold,
-                  color: kPrimaryColor,
-                ),
-              ),
-            ],
+          Text(
+            'Welcome back, ${userName ?? 'there'} 👋',
+            style: TextStyle(
+              fontSize: 22.sp,
+              fontWeight: FontWeight.bold,
+              color: kPrimaryColor,
+            ),
           ),
           SizedBox(height: 6.h),
           Text(
-            "Let’s explore something creative today!",
+            "Let's explore something creative today!",
             style: TextStyle(fontSize: 14.sp, color: kSubTitleColor),
           ),
           SizedBox(height: 20.h),
           Text(
             'Features',
             style: TextStyle(
-              fontSize: 20.sp,
+              fontSize: 24.sp,
               fontWeight: FontWeight.bold,
               color: kPrimaryColor,
             ),
